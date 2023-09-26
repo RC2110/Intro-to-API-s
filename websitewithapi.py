268c8fee0b37e44e977ad6db9baafbe8ef79bca4
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-09-26 06:56:00 +0530

    Created a webapp that uses API to fetch information from NASA

---
+++ websitewithapi.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+key = "Euz89cBPl9q6HZ0UuDZcbHvRzaGgDbxOaznm0vqz"
+url = f"https://api.nasa.gov/planetary/apod?api_key={key}"
+imageurl="https://apod.nasa.gov/apod/image/2309/Arp142_HubbleChakrabarti_2627.jpg"
+request = requests.get(url)
+data = request.json()
+
+request1= requests.get(imageurl)
+imgdata = request1.content
+
+with open ("nasaimg.jpeg", 'wb') as file:
+    file.write(imgdata)
+
+st.set_page_config(layout="centered")
+st.header("Pic & News of the day")
+st.image("nasaimg.jpeg")
+st.write(data['explanation'])
+
+
+
+
+
